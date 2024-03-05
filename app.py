d53de27bb92ae88c55eceb029fcb25f679b97118
Nayeli Montiel <133909181+Naye013@users.noreply.github.com>
2024-03-04 16:45:09 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,11 @@
-import altair as alt
 import dash
-from dash import dcc as dcc
-from dash import html as html
-from vega_datasets import data
+from dash import html
 
-cars = data.cars()
-brush = alt.selection_interval()
-chart = (alt.Chart(cars).mark_circle().encode(
-    x='Horsepower',
-    y='Miles_per_Gallon',
-    color=alt.condition(brush, 'Origin', alt.value('lightgray')))
-.add_params(brush))
+myapp = dash.Dash()
 
-app = dash.Dash(__name__)
-server = app.server
-app.layout = html.Div([
-        html.Iframe(
-            srcDoc=chart.to_html(),
-            style={'border-width': '0', 'width': '100%', 'height': '400px'})])
+myapp.layout = html.Div(
+    html.H1(children="Hello World")
+)
 
-if __name__ == '__main__':
-    app.run_server(host='0.0.0.0', port=8050, debug=True)
+if __name__=="__main__":
+    myapp.run_server(host='0.0.0.0', port=8050, debug=True)
